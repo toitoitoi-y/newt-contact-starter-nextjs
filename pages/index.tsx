ea6fb408b5e82c856060c8bbcfa44e42d51b90fc
atsushifujikawa <atsushi.fujikawa@newt.so>
2022-11-09 12:05:27 +0900

    implement

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,47 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import router from "next/router";
+import styles from "../styles/Home.module.css";
 import { Layout } from "../components/Layout";
+import { useCallback, useState } from "react";
+
+type SubmissionData = {
+  name: string;
+  email: string;
+  comment: string;
+};
 
 const Home: NextPage = () => {
+  const [data, setData] = useState<SubmissionData>({
+    name: "",
+    email: "",
+    comment: "",
+  });
+
+  const onSubmit = useCallback(async (data: SubmissionData) => {
+    if (!data.name || !data.email || !data.comment) {
+      alert("Please fill in all fields.");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("email", data.email);
+    formData.append("comment", data.comment);
+    await fetch(process.env.NEXT_PUBLIC_NEWT_FORM_ENDPOINT as string, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => {
+        router.push("/complete");
+      })
+      .catch((err) => {
+        router.push("/complete_error");
+      });
+  }, []);
+
   return (
     <>
       <Head>
@@ -12,20 +50,55 @@ const Home: NextPage = () => {
       <Layout>
         <div className={styles.Contact}>
           <h1 className={styles.Contact_Title}>Contact us</h1>
-          <p className={styles.Contact_Text}>Please fill this form in a decent manner.</p>
+          <p className={styles.Contact_Text}>
+            Please fill this form in a decent manner.
+          </p>
           <form className={styles.Contact_Form}>
             <label className={styles.Contact_Label}>Full Name*</label>
-            <input type="text" className={styles.Contact_InputText} required />
+            <input
+              type="text"
+              className={styles.Contact_InputText}
+              value={data.name}
+              onChange={(e) => {
+                setData({ ...data, name: e.target.value });
+              }}
+              required
+            />
             <label className={styles.Contact_Label}>Email*</label>
-            <input type="email" className={styles.Contact_InputText} required />
+            <input
+              type="email"
+              className={styles.Contact_InputText}
+              value={data.email}
+              onChange={(e) => {
+                setData({ ...data, email: e.target.value });
+              }}
+              required
+            />
             <label className={styles.Contact_Label}>Comment*</label>
-            <textarea className={styles.Contact_Textarea} rows={4} required></textarea>
-            <button type="submit" className={styles.Contact_Button}>Submit</button>
+            <textarea
+              className={styles.Contact_Textarea}
+              rows={4}
+              value={data.comment}
+              onChange={(e) => {
+                setData({ ...data, comment: e.target.value });
+              }}
+              required
+            ></textarea>
+            <button
+              type="submit"
+              className={styles.Contact_Button}
+              onClick={(e) => {
+                e.preventDefault();
+                onSubmit(data);
+              }}
+            >
+              Submit
+            </button>
           </form>
         </div>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
