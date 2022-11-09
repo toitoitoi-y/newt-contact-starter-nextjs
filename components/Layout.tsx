288a34ce4c7913b65b19bb6ba3b0d43039779b4b
atsushifujikawa <atsushi.fujikawa@newt.so>
2022-11-09 12:12:21 +0900

    fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,14 @@
+import { ReactNode } from "react";
 import styles from "../styles/Layout.module.css";
 import { Badge } from "./Badge";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 
-export function Layout({
-  children,
-}): JSX.Element {
+export function Layout({ children }: { children: ReactNode }): JSX.Element {
   return (
     <div className={styles.Wrapper}>
       <Header />
-      <main className={styles.Container}>
-        {children}
-      </main>
+      <main className={styles.Container}>{children}</main>
       <Footer />
       <Badge />
     </div>
